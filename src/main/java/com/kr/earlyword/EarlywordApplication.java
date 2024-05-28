2e79d6e6b8b8caed9aa4e8012defff12ea4032d7
ksngh <chzhqk98@naver.com>
2024-05-28 09:54:54 +0900

    initial commit

---
+++ src/main/java/com/kr/earlyword/EarlywordApplication.java
@@ -0,0 +1,13 @@
+package com.kr.earlyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlywordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarlywordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/earlyword/EarlywordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.earlyword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlywordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
