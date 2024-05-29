5c2025a12dcdf3261e75acf3deb182bbf3f6094e
ksngh <chzhqk98@naver.com>
2024-05-29 11:30:48 +0900

    feat : CustomOAuth2UserService 생성

--- src/main/java/com/earlyword/service/CustomOAuth2UserService.java
+++ src/main/java/com/earlyword/service/CustomOAuth2UserService.java
@@ -1,5 +1,18 @@
 package com.earlyword.service;
 
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import jakarta.servlet.http.HttpSession;
+
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
