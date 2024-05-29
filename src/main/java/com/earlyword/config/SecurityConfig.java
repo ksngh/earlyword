e49e90f1fba5b87587800017d882bd76b7624efb
ksngh <chzhqk98@naver.com>
2024-05-29 11:22:10 +0900

    feat : SecurityConfig 생성

---
+++ src/main/java/com/earlyword/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.earlyword.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.earlyword.service.CustomOAuth2UserService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig { //WebSecurityConfigurerAdapter was deprecated
+
+	private final CustomOAuth2UserService customOAuth2UserService;
+
+	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+		this.customOAuth2UserService = customOAuth2UserService;
+	}
+
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.formLogin().disable()
+			.httpBasic().disable()
+			.authorizeRequests()
+			.antMatchers("/api/user").permitAll()
+			.and()
+			.oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+		return http.build();
+	}
+}
