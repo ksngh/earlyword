831f38c3f9003d692883a6a4af52b3cca12362c8
ksngh <chzhqk98@naver.com>
2024-07-03 17:46:20 +0900

    chore : 회원가입 & 로그인 -> kakao OAUTH
    service 디렉토리 생성, url 관리할 KakaoConnection 클래스 생성

---
+++ src/main/java/com/earlyword/service/MemberService.java
@@ -0,0 +1,4 @@
+package com.earlyword.service;
+
+public class MemberService {
+}
---
+++ src/main/java/com/earlyword/service/OAuthService/KakaoConnection.java
@@ -0,0 +1,11 @@
+package com.earlyword.service.OAuthService;
+
+import org.springframework.stereotype.Service;
+
+import lombok.Value;
+
+@Service
+public class KakaoConnection {
+	// @Value("${kakao.client-id}")
+	private String clientId;
+}
